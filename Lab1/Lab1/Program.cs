efe40ad408e2795f7dd5e1b967a2b226fed0cf46
Przemysław Szułczyński <pszulczynski@edu.cdv.pl>
2025-10-11 13:42:39 +0200

    Zadanie od 1 do 3

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,3 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Task 1
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Podaj hasło!");
+string password = Console.ReadLine();
+
+do{
+    if (password == "admin123")
+    {
+        Console.WriteLine("Podałeś prawidłowe hasło!");
+    }
+    else
+    {
+        Console.WriteLine("Podałeś nieprawidłowe hasło spróbuj ponownie!");
+        password = Console.ReadLine();
+        if (password == "admin123")
+        {
+            Console.WriteLine("Podałeś prawidłowe hasło!");
+        }
+    }
+}while(password != "admin123");
+
+
+// Task 2
+
+Console.WriteLine("Podaj liczbę większą od zera!");
+string positiveNumber = Console.ReadLine();
+int okNumber = int.Parse(positiveNumber);
+bool flag = true;
+
+do
+{
+    
+    if (okNumber > 0)
+    {
+        Console.WriteLine("Podałeś prawidłową liczbę!");
+        flag = false;
+    }
+    else
+    {
+        if (okNumber == 0)
+        {
+            Console.WriteLine("Podałeś nieprawidłową liczbę - zero! Spróbuj ponownie!");
+            positiveNumber = Console.ReadLine();
+            okNumber = int.Parse(positiveNumber);
+        }
+        else
+        {
+            Console.WriteLine("Podałeś nieprawidłową liczbę - ujemną! Spróbuj ponownie!");
+            positiveNumber = Console.ReadLine();
+            okNumber = int.Parse(positiveNumber);
+        }
+    }
+
+}while(flag);
+
+
+// Task 3
+string[] miasta = {"Warszawa","Berlin","Moskwa","Londyn","Oslo"}
+;
+
+foreach (string nazwy in miasta)
+{
+    Console.WriteLine(nazwy);
+}
